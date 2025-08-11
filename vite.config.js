6044cea338f794bdf94fe875c388995fb51d6800
Andrea Benavides <andrepaobena@gmail.com>
2025-08-11 14:23:31 -0600

    cambios

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import fs from "fs";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.js"],
+            input: ["resources/js/app.js", "resources/css/app.css"],
             refresh: true,
         }),
     ],
